5db592c87c2118b637dc79ceafa9d5fa069cd4b0
Justin4587 <Thurman@atlasok.com>
2021-09-17 09:34:41 -0500

    godbolt is great

--- 0x05-libasm/0-strlen.asm
+++ 0x05-libasm/0-strlen.asm
@@ -1,1 +1,22 @@
 BITS 64
+        global asm_strlen
+        section .text
+asm_strlen:
+        push    rbp
+        mov     rbp, rsp
+        mov     QWORD [rbp-24], rdi
+        mov     QWORD [rbp-8], 0
+        jmp     .L2
+.L3:
+        add     QWORD [rbp-8], 1
+.L2:
+        mov     rax, QWORD [rbp-8]
+        mov     rdx, QWORD [rbp-24]
+        add     rax, rdx
+        movzx   eax, BYTE [rax]
+        test    al, al
+        jne     .L3
+        mov     rax, QWORD [rbp-8]
+        pop     rbp
+        ret
+    
